1fcf2c799f8d6e8426dc86d40bc7f7f64edc6f08
filippohd <filippogarganohd@gmail.com>
2025-12-10 22:09:41 +0100

    fixed

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,17 +1,45 @@
 <script setup>
   import ErrorMsg from '../components/ErrorMsg.vue'
   import {useRouter} from 'vue-router'
-  import axios from 'axios'
+  import axios from '../services/axios.js'
+  import {ref} from 'vue' 
 
   const router = useRouter();
+
+  const User = ref({
+    Username: '',
+    UserId: ''
+  })
+
+  const Authorization = ref(null)
+  
   
   const doLogin = async () => {
 
     try {
-      const response = await axios.post('/login');
+      
+      const response = await axios.post('/login', {
+        Username: User.Username
+      });
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      
+      User.UserId = response.data.Identifier
+
+      localStorage.setItem('userId', User.UserId);
+      localStorage.setItem('username', User.Username);
+      localStorage.setItem('Authorization', `Bearer ${User.UserId}`);
+
       router.push("/home")
+
+      axios.defaults.headers['Authorization'] = `Bearer ${User.UserId}`;
+
     } catch (e) {
-      alert(e);
+      console.error(e)
+      alert('Errore durante il login: ' + e.message)
     }
   };
   
@@ -26,7 +54,7 @@
 
   <div class="text-center">
     <div class="text-center">Login</div>
-    <input v-model="username" placeholder="Type here" />
+    <input v-model="User.Username" placeholder="Type here" />
     <button type="button" class="btn btn-primary" @click="doLogin">Go</button>
   </div>
 </template>
